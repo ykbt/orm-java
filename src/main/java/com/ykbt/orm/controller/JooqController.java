60ed9e65d496ec40a1d068982d70982752afe29f
Yusuke Kubota <yusuke.kubota@gmo.media>
2018-11-12 20:19:31 +0900

    とりあえず追加してみたやつ

---
+++ src/main/java/com/ykbt/orm/controller/JooqController.java
@@ -0,0 +1,57 @@
+package com.ykbt.orm.controller;
+
+import com.ykbt.orm.resource.UserResource;
+import com.ykbt.orm.service.OrmService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/jooq/users")
+public class JooqController {
+    @Autowired
+    @Qualifier("jooqServiceImpl")
+    OrmService service;
+
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public UserResource getUser(@PathVariable("id") Integer id) {
+        return service.getUser(id);
+    }
+
+    @RequestMapping(method = RequestMethod.GET, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public List<UserResource> getUsers() {
+        return service.getUsers();
+    }
+
+    @RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public UserResource addUser(@RequestBody UserResource resource) {
+        return service.addUser(resource);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public UserResource updateUser(@RequestBody UserResource resource) {
+        return service.updateUser(resource);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public UserResource updateUser(@PathVariable("id") Integer id, @RequestBody UserResource resource) {
+        resource.setId(id);
+        return service.updateUser(resource);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public String deleteUser(@PathVariable("id") Integer id) {
+        service.deleteUser(id);
+        return "OK";
+    }
+}
---
+++ src/main/java/com/ykbt/orm/service/JooqServiceImpl.java
@@ -0,0 +1,83 @@
+package com.ykbt.orm.service;
+
+import com.ykbt.orm.entity.UserEntity;
+import com.ykbt.orm.repository.JooqRepository;
+import com.ykbt.orm.resource.UserResource;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class JooqServiceImpl implements OrmService {
+    @Autowired
+    private JooqRepository repository;
+
+    public List<UserResource> getUsers() {
+        List<UserEntity> entities = repository.findAll();
+        List<UserResource> resources = new ArrayList<>();
+        entities.stream().forEach(entity -> {
+            UserResource resource = new UserResource();
+            BeanUtils.copyProperties(entity, resource);
+            resources.add(resource);
+        });
+        return resources;
+    }
+
+    public UserResource getUser(Integer id) {
+        if(id == null){
+            return null;
+        }
+
+        UserEntity entity = repository.get(id);
+        UserResource resource = new UserResource();
+        BeanUtils.copyProperties(entity, resource);
+
+        return resource;
+    }
+
+    @Transactional
+    public UserResource addUser(UserResource resource) {
+        if (resource == null || resource.getId() != null) {
+            return null;
+        }
+
+        UserEntity entity = new UserEntity();
+        BeanUtils.copyProperties(resource, entity);
+
+        if (repository.save(entity) != 1){
+            throw new RuntimeException("insertに失敗しました Entity:" + entity);
+        }
+
+        UserEntity addedEntity = repository.get(entity.getId());
+        UserResource addedResouce = new UserResource();
+        BeanUtils.copyProperties(addedEntity, addedResouce);
+
+        return addedResouce;
+    }
+
+    @Transactional
+    public UserResource updateUser(UserResource resource) {
+        if (resource == null || resource.getId() == null) {
+            return null;
+        }
+
+        UserEntity entity = new UserEntity();
+        BeanUtils.copyProperties(resource, entity);
+
+        repository.update(entity);
+        UserEntity updatedEntity = repository.get(entity.getId());
+        UserResource updatedResource = new UserResource();
+        BeanUtils.copyProperties(updatedEntity, updatedResource);
+
+        return updatedResource;
+    }
+
+    @Transactional
+    public void deleteUser(Integer id) {
+        repository.delete(id);
+    }
+}
